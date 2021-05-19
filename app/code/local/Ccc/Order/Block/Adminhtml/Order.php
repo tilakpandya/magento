5825fae0ac40e268c644c56bbf667785140c167b
unknown <tilakpandya2@gmail.com>
2021-05-19 11:02:46 +0530

    Third Commit

--- app/code/local/Ccc/Order/Block/Adminhtml/Order.php
+++ app/code/local/Ccc/Order/Block/Adminhtml/Order.php
@@ -6,6 +6,7 @@ class Ccc_Order_Block_Adminhtml_Order extends Mage_Adminhtml_Block_Widget_Grid_C
         echo 111; 
         die;
         $this->_controller = 'adminhtml_order';
+        $this->_blockGroup = 'order';
         $this->_headerText = Mage::helper('sales')->__('Orders');
         $this->_addButtonLabel = Mage::helper('sales')->__('Create New Order');
         parent::__construct();
